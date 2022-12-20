e1b97f287482a4f79bd3d0d2c209a9b089257269
Juri Pakaste <juri@juripakaste.fi>
2022-12-20 21:38:59 +0200

    Add a TestGraphics Graphics type
    Collects the commands

--- Tests/BoxesAndArrowsTests/TestGraphics.swift
+++ Tests/BoxesAndArrowsTests/TestGraphics.swift
@@ -1,8 +1,30 @@
-//
-//  File.swift
-//  
-//
-//  Created by Juri Pakaste on 20.12.2022.
-//
-
+import Draw
 import Foundation
+
+struct TestGraphics {
+    static let lineHeight: Double = 20.0
+    static let charWidth: Double = 10.0
+}
+
+extension TestGraphics: Graphics {
+    func measure(attributedString: AttributedString) -> Draw.Size {
+        let chars = attributedString.characters
+        let lines = chars.split(separator: "\n")
+        let maxLineLength = lines.map(\.count).max()!
+        return Size(width: Double(maxLineLength) * Self.charWidth, height: Double(lines.count) * Self.lineHeight)
+    }
+
+    func makeDrawing(size: Size) -> any Drawing<[DrawCommand]> {
+        TestDrawing(size: size)
+    }
+}
+
+struct TestDrawing {
+    let size: Size
+}
+
+extension TestDrawing: Drawing {
+    func draw(_ commands: [DrawCommand]) -> [DrawCommand] {
+        commands
+    }
+}
