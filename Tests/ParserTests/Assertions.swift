cd94cc9ec5a8dab9e9525533995902fd8912a0a8
Juri Pakaste <juri@juripakaste.fi>
2023-01-06 11:44:08 +0200

    Make collection comparison assertion more useful
    Better support for inequal sizes

--- Tests/ParserTests/Assertions.swift
+++ Tests/ParserTests/Assertions.swift
@@ -6,10 +6,13 @@ func assertEqual<C>(
     file: StaticString = #filePath,
     line: UInt = #line
 ) where C: Collection, C.Element: Equatable {
-    XCTAssertEqual(col1.count, col2.count, "Expected equal number of elements", file: file, line: line)
-    guard col1.count == col2.count else { return }
     for (index, (e1, e2)) in zip(0..., zip(col1, col2)) {
         XCTAssertEqual(e1, e2, "Expected equal elements at index \(index)", file: file, line: line)
-        guard e1 == e2 else { return }
+    }
+    for (index, e1) in col1.enumerated().dropFirst(col2.count) {
+        XCTFail("Missing element in col2 at index \(index): \(e1)", file: file, line: line)
+    }
+    for (index, e2) in col2.enumerated().dropFirst(col1.count) {
+        XCTFail("Missing element in col1 at index \(index): \(e2)", file: file, line: line)
     }
 }
