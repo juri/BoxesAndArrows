e3c7d2be5e516a0dcc062df6e290a7f04b214cb0
Juri Pakaste <juri@juripakaste.fi>
2023-01-06 12:06:30 +0200

    Extract color parsing to a separate file

---
+++ Sources/Parser/ColorParser.swift
@@ -0,0 +1,54 @@
+import Draw
+import Parsing
+
+struct HexByte: ParserPrinter {
+    func parse(_ input: inout Substring.UTF8View) throws -> UInt8 {
+        let prefix = input.prefix(2)
+        guard
+            prefix.count == 2,
+            let byte = UInt8(String(decoding: prefix, as: UTF8.self), radix: 16)
+        else { throw ParsingError() }
+        input.removeFirst(2)
+        return byte
+    }
+
+    func print(_ output: UInt8, into input: inout Substring.UTF8View) {
+        let byte = String(output, radix: 16)
+        input.prepend(contentsOf: byte.count == 1 ? "0\(byte)".utf8 : "\(byte)".utf8)
+    }
+}
+
+enum ColorFieldID: String, CaseIterable {
+    case backgroundColor = "background-color"
+    case textColor = "text-color"
+}
+
+let colorParse = Parse {
+    ColorFieldID.parser()
+    Whitespace(.horizontal)
+    ":".utf8
+    Whitespace(.horizontal)
+    hexColor
+}
+
+let hexColor = ParsePrint(.memberwise(hcolor(red:green:blue:alpha:))) {
+    "#".utf8
+    HexByte()
+    HexByte()
+    HexByte()
+    HexByte()
+}
+
+func hcolor(
+    red: UInt8,
+    green: UInt8,
+    blue: UInt8,
+    alpha: UInt8
+) -> Color {
+    Color(
+        red: Double(red) / 255.0,
+        green: Double(green) / 255.0,
+        blue: Double(blue) / 255.0,
+        alpha: Double(alpha) / 255.0
+    )
+}
--- Sources/Parser/Parser.swift
+++ Sources/Parser/Parser.swift
@@ -22,28 +22,6 @@ import Parsing
 
 struct ParsingError: Error {}
 
-struct HexByte: ParserPrinter {
-    func parse(_ input: inout Substring.UTF8View) throws -> UInt8 {
-        let prefix = input.prefix(2)
-        guard
-            prefix.count == 2,
-            let byte = UInt8(String(decoding: prefix, as: UTF8.self), radix: 16)
-        else { throw ParsingError() }
-        input.removeFirst(2)
-        return byte
-    }
-
-    func print(_ output: UInt8, into input: inout Substring.UTF8View) {
-        let byte = String(output, radix: 16)
-        input.prepend(contentsOf: byte.count == 1 ? "0\(byte)".utf8 : "\(byte)".utf8)
-    }
-}
-
-enum ColorFieldID: String, CaseIterable {
-    case backgroundColor = "background-color"
-    case textColor = "text-color"
-}
-
 enum NumericFieldID: String, CaseIterable {
     case lineWidth = "line-width"
 }
@@ -62,14 +40,6 @@ struct ColorField: Equatable {
 struct VariableField: Equatable {
     let fieldID: VariableFieldID
     let value: String
-}
-
-let colorParse = Parse {
-    ColorFieldID.parser()
-    Whitespace(.horizontal)
-    ":".utf8
-    Whitespace(.horizontal)
-    hexColor
 }
 
 let numberParse = Parse(.memberwise(NumericField.init(fieldID:value:))) {
@@ -459,26 +429,4 @@ extension TopLevelDecl.Connection {
 
         func unapply(_ output: Output) throws -> Input { (output.node1[...], output.node2[...], output.fields) }
     }
-}
-
-let hexColor = ParsePrint(.memberwise(hcolor(red:green:blue:alpha:))) {
-    "#".utf8
-    HexByte()
-    HexByte()
-    HexByte()
-    HexByte()
-}
-
-func hcolor(
-    red: UInt8,
-    green: UInt8,
-    blue: UInt8,
-    alpha: UInt8
-) -> Color {
-    Color(
-        red: Double(red) / 255.0,
-        green: Double(green) / 255.0,
-        blue: Double(blue) / 255.0,
-        alpha: Double(alpha) / 255.0
-    )
 }
