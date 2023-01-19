95d144ede388dc2c91ae0079dc92d10e96343a42
Juri Pakaste <juri@juripakaste.fi>
2023-01-19 18:26:37 +0200

    Reduce nesting

--- Sources/Draw/TextAttributes.swift
+++ Sources/Draw/TextAttributes.swift
@@ -1,24 +1,38 @@
 import Foundation
 
 extension AttributeScopes {
-    public var boxesAndArrows: AttributeScopes.BoxesAndArrowsAttributes.Type {
+    public var boxesAndArrows: BoxesAndArrowsAttributes.Type {
         BoxesAndArrowsAttributes.self
-    }
-
-    public struct BoxesAndArrowsAttributes: AttributeScope {
-        public let textColor: AttributeScopes.BoxesAndArrowsAttributes.TextColorAttribute
     }
 }
 
-extension AttributeScopes.BoxesAndArrowsAttributes {
-    public enum TextColorAttribute: AttributedStringKey {
+public struct BoxesAndArrowsAttributes: AttributeScope {
+    public let textAlignment: BoxesAndArrowsAttributes.TextAlignmentAttribute
+    public let textColor: BoxesAndArrowsAttributes.TextColorAttribute
+    public let font: BoxesAndArrowsAttributes.FontAttribute
+}
+
+public protocol BoxesAndArrowsAttributedStringKey: AttributedStringKey {}
+
+extension BoxesAndArrowsAttributes {
+    public enum TextAlignmentAttribute: BoxesAndArrowsAttributedStringKey {
+        public typealias Value = TextAlignment
+        public static let name = "TextAlignment"
+    }
+
+    public enum TextColorAttribute: BoxesAndArrowsAttributedStringKey {
         public typealias Value = Color
         public static let name = "TextColor"
+    }
+
+    public enum FontAttribute: BoxesAndArrowsAttributedStringKey {
+        public typealias Value = Font
+        public static let name = "Font"
     }
 }
 
 public extension AttributeDynamicLookup {
-    subscript<T: AttributedStringKey>(dynamicMember keyPath: KeyPath<AttributeScopes.BoxesAndArrowsAttributes, T>) -> T {
+    subscript<T: AttributedStringKey>(dynamicMember keyPath: KeyPath<BoxesAndArrowsAttributes, T>) -> T {
         self[T.self]
     }
 }
