3cfed3f79a08558618c8acb92fc48f2c39c82a5f
Juri Pakaste <juri@juripakaste.fi>
2023-02-05 14:52:48 +0200

    Remove default comments from Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -7,7 +6,6 @@ let package = Package(
     name: "BoxesAndArrows",
     platforms: [.macOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "BoxesAndArrows",
             targets: ["BoxesAndArrows"]
@@ -22,8 +20,6 @@ let package = Package(
         .package(url: "https://github.com/pointfreeco/swift-tagged.git", from: "0.8.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "BoxesAndArrows",
             dependencies: [
